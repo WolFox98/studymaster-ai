c52d51f38bb6b8782fae4c7112a460cf0358cc55
WolFox <120948856+WolFox98@users.noreply.github.com>
2025-06-02 18:38:05 +0200

    Add StudyMasterApp/webpack.config.js 1748882283227

--- StudyMasterApp/webpack.config.js
+++ StudyMasterApp/webpack.config.js
@@ -1,17 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'index.web.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    alias: {
-      'react-native$': 'react-native-web',
-    },
-    extensions: ['.web.js', '.js'],
   },
   module: {
     rules: [
@@ -19,21 +15,36 @@ module.exports = {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['module:metro-react-native-babel-preset'],
-          },
+          loader: 'babel-loader'
         },
-      },
+      }
     ],
   },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    compress: true,
-    port: 8000,
-    hot: true,
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+    extensions: ['.web.js', '.js'],
+    fallback: {
+      "crypto": false,
+      "stream": false,
+      "path": false,
+      "fs": false
+    }
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'dist/index.html')
+    }),
+    new webpack.DefinePlugin({
+      process: { env: {} }
+    })
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8000,
+    hot: true
+  }
 };
