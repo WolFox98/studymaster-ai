e0446809c9cc1d36695549a64611b53a3c6d8595
WolFox <120948856+WolFox98@users.noreply.github.com>
2025-06-02 04:30:46 +0200

    Add StudyMasterApp/index.web.js 1748831445748

---
+++ StudyMasterApp/index.web.js
@@ -0,0 +1,10 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+const appName = 'StudyMasterApp';
+
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'),
+});
