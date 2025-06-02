f0f74b52a994c44892a729f1f526e9fa51802776
WolFox <120948856+WolFox98@users.noreply.github.com>
2025-06-02 04:30:56 +0200

    Add StudyMasterApp/windows/StudyMasterApp/MainPage.h 1748831456355

---
+++ StudyMasterApp/windows/StudyMasterApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::StudyMasterApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::StudyMasterApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
